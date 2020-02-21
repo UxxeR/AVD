0080860368d736d3ffac754afe2f22cf578789bd
Erik Font <erikfont96@gmail.com>
2020-02-21 19:04:00 +0100

    menu finished

---
+++ AVD/Assets/Erik's folder/Final project/LoadScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadScene : MonoBehaviour
+{
+    // Update is called once per frame
+    public void LoadSceneLevel(int level)
+    {
+            SceneManager.LoadSceneAsync(level);
+    }
+}
