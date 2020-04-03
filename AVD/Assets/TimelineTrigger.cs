f3b5b18e07e9ea62060f49f3e6dfcd4211cfb017
Erik Font <erikfont96@gmail.com>
2020-04-03 19:29:28 +0200

    3 april

---
+++ AVD/Assets/TimelineTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelineTrigger : MonoBehaviour
+{
+    private PlayableDirector pb;
+    // Start is called before the first frame update
+    void Start()
+    {
+       pb = GetComponent<PlayableDirector>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        pb.Play();
+    }
+}
